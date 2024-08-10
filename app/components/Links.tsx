84e613072b75fded9406f0421f21213cb2bfd32d
Jorge Matos <jisapjisap@gmail.com>
2024-08-10 18:30:27 +0200

    liniks

--- app/components/Links.tsx
+++ app/components/Links.tsx
@@ -2,24 +2,25 @@ import React from 'react'
 import HomeIcon from '@mui/icons-material/Home';
 import CategoryIcon from '@mui/icons-material/Category';
 import FavoriteIcon from '@mui/icons-material/Favorite';
+import { useAppContext } from '../ContextApi';
 
 const Links = () => {
+
+  const { menuItemsObject: { menuItems }} = useAppContext()
+
   return (
     <div className='mt-44 ml-3 flex flex-col gap-2 text-[15px]'>
-      <div className='p-[7px] hover:text-sky-500 select-none cursor-pointer rounded-lg flex items-center gap-2 w-[75%] bg-sky-500 text-white'>
-        <HomeIcon />
-        <span className='mt-0.5'>Home</span>
-      </div>
+      
+      {menuItems.map((item, index) => (
+        <div
+          key={index}
+          className={`${item.isSelected ? "bg-sky-500 text-white" : "text-slate-400 hover:text-sky-500"} p-[7px] select-none cursor-pointer rounded-lg flex items-center gap-2 w-[75%]`}
+        >
+          {item.icon}
+          <span className='mt-0.5'>{item.name}</span>
+        </div>
+      ))}
 
-      <div className='p-[7px] hover:text-sky-500 select-none cursor-pointer rounded-lg flex items-center gap-2 w-[75%] text-slate-400'>
-        <CategoryIcon />
-        <span className='mt-0.5'>Categories</span>
-      </div>
-
-      <div className='p-[7px] hover:text-sky-500 select-none cursor-pointer rounded-lg flex items-center gap-2 w-[75%] text-slate-400'>
-        <FavoriteIcon />
-        <span className='mt-0.5'>Favorites</span>
-      </div>
     </div>
   )
 }
