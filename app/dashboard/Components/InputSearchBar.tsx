dc1dd17dfce243b44963047babc2e01d2655a589
Jorge Matos <jisapjisap@gmail.com>
2024-08-17 19:49:35 +0200

    Search

--- app/dashboard/Components/InputSearchBar.tsx
+++ app/dashboard/Components/InputSearchBar.tsx
@@ -1,8 +1,36 @@
 "use client"
 
+import { useAppContext } from "@/app/ContextApi"
+import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
+import { useEffect, useRef } from "react";
+
 const InputSearchBar = () => {
+
+  const { showSearchBarObject: { setShowSearchBar }} = useAppContext();
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    inputRef.current?.focus();
+  },[]);
+
+  const handleCloseClick = (e: React.MouseEvent) => {
+    e.stopPropagation();
+    setShowSearchBar(false);
+  };
+
   return (
-    <div>InputSearchBar</div>
+    <div className="px-2 flex justify-between items-center w-full">
+      <input 
+        ref={inputRef}
+        placeholder="Search a component"
+        className="w-full bg-slate-100 outline-none text-[-13px] placeholder:text-slate-400"
+      />
+      <CloseRoundedIcon 
+        fontSize="small"
+        className="text-slate-500 text-[10px] cursor-pointer"
+        onClick={handleCloseClick}
+      />
+    </div>
   )
 }
 
