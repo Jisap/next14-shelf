1e6e536de35f77cf98b1f210dfb6bb6f747573ca
Jorge Matos <jisapjisap@gmail.com>
2024-08-18 10:59:02 +0200

    useUser

--- app/dashboard/Components/DashboardText.tsx
+++ app/dashboard/Components/DashboardText.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+"use client"
+
+import { useUser } from '@clerk/nextjs'
+
 
 const DashboardText = () => {
+
+  const { user } = useUser();
+
   return (
     <div className='flex flex-col'>
-      <span className='font-semibold'>Welcome Back, Pedro</span>
+      <span className='font-semibold'>Welcome Back, {user?.lastName}</span>
       <span className='text-slate-400 text-[11px] font-light'>
         We are happy to see you again
       </span>
--- app/dashboard/Components/ProfileAccount.tsx
+++ app/dashboard/Components/ProfileAccount.tsx
@@ -1,9 +1,12 @@
+import { UserButton } from '@clerk/nextjs'
 import React from 'react'
 
 const ProfileAccount = () => {
   return (
     <div className='flex gap-3 items-center'>
-      <div className='w-[36px] h-[37px] bg-slate-100 rounded-full'></div>
+      <div className='w-[36px] h-[37px] bg-slate-100 rounded-full flex items-center justify-center'>
+        <UserButton />
+      </div>
     </div>
   )
 }
