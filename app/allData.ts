fed6269fada977ba2237932050c8433735b6efec
Jorge Matos <jisapjisap@gmail.com>
2024-08-22 18:42:30 +0200

    allData.ts

---
+++ app/allData.ts
@@ -0,0 +1,55 @@
+import { v4 as uuidv4 } from 'uuid';
+
+export interface Components {
+  _id:          string;
+  name:         string;
+  projectName:  string;
+  code:         string;
+  isFavorite:   boolean;
+  createdAt:    string;
+}
+
+export interface Project {
+  _id:          string;
+  clerkUserId:  string;
+  name:         string;
+  icon:         string;
+  createdAt:    string;
+  components:   Components[];
+}
+
+export const allProjectsData: Project[] = [
+  {
+    _id: uuidv4(),
+    clerkUserId: "",
+    name: "Forms",
+    icon: "categoryIcon",
+    createdAt: "2022-01-01T00:00:00.000Z",
+    components: [
+      {
+        _id: uuidv4(),
+        name: "Forms 1",
+        projectName: "Forms",
+        code: "",
+        isFavorite: false,
+        createdAt: "2022-01-01T00:00:00.000Z"
+      },
+      {
+        _id: uuidv4(),
+        name: "Forms 2",
+        projectName: "Forms",
+        code: "",
+        isFavorite: true,
+        createdAt: "2022-02-01T00:00:00.000Z"
+      },
+      {
+        _id: uuidv4(),
+        name: "Forms 3",
+        projectName: "Forms",
+        code: "",
+        isFavorite: true,
+        createdAt: "2022-02-01T00:00:00.000Z"
+      }
+    ]
+  }
+]
