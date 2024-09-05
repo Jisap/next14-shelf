5a449f93e4f11aff8235e2ab74b8cf954b5004d0
Jorge Matos <jisapjisap@gmail.com>
2024-09-05 16:49:35 +0200

    textToIcon update

--- utils/textToIcon.tsx
+++ utils/textToIcon.tsx
@@ -1,5 +1,7 @@
 import CategoryIcon from '@mui/icons-material/Category';
 import RectangleIcon from '@mui/icons-material/Rectangle';
+import CodeOutlinedIcon from '@mui/icons-material/CodeOutlined';
+import DeveloperModeOutlinedIcon from '@mui/icons-material/DeveloperModeOutlined';
 
 export const TextToIcon = ({ text, size }: { text:string; size?:"small" | "medium" | "large" }) => {
   
@@ -9,6 +11,12 @@ export const TextToIcon = ({ text, size }: { text:string; size?:"small" | "mediu
     
     case "RectangleIcon":
       return <RectangleIcon fontSize={size} className='text-sky-400' />
+    
+    case "CodeIcon":
+      return <CodeOutlinedIcon fontSize={size} className='text-sky-400' />
+
+    case "DeveloperIcon":
+      return <DeveloperModeOutlinedIcon fontSize={size} className='text-sky-400' />
 
     default:
       return <CategoryIcon fontSize={size} className='text-sky-400' />
