9344727d3ee1b785c8fc51734e216a62bfd88708
Jorge Matos <jisapjisap@gmail.com>
2024-09-22 15:28:57 +0200

    delete button

--- app/dashboard/Components/SingleFavoriteComponent.tsx
+++ app/dashboard/Components/SingleFavoriteComponent.tsx
@@ -7,18 +7,19 @@ import { useAppContext } from '@/app/ContextApi';
 const SingleFavoriteComponent = ({ component }: { component:Component }) => {
   
   const {
-    selectedComponentObject: { selectedComponent, setSelectedComponent },
-    openComponentEditorObject: { openComponentEditor, setOpenComponentEditor },
-    selectedProjectObject: { selectedProject, setSelectedProject },
+    selectedComponentObject: { setSelectedComponent },
+    openComponentEditorObject: { setOpenComponentEditor },
+    selectedProjectObject: { setSelectedProject },
     allProjectsObject: { allProjects },
+    openDeleteWindowObject: { setOpenDeleteWindow },
   } = useAppContext();
   
 
   const openComponent = () => {
     setSelectedComponent(component);
     setOpenComponentEditor(true)
-    // Get the project and set it in the selectedProject state
-    const project = allProjects.find(
+    
+    const project = allProjects.find(                                           // Get the project and set it in the selectedProject state                      
       (project) =>
         project.name.toLowerCase() === component.projectName.toLowerCase()
     );
@@ -29,8 +30,23 @@ const SingleFavoriteComponent = ({ component }: { component:Component }) => {
       console.log(`Project not found for component: ${component.name}`);
     }
 
-    // Scroll to the top of the page or to the component editor
-    window.scrollTo({ top: 0, behavior: "smooth" });
+    window.scrollTo({ top: 0, behavior: "smooth" });                             // Scroll to the top of the page or to the component editor
+  }
+
+  const openTheDeleteWindow = () => {
+    const project = allProjects.find(
+      (project) => 
+        project.name.toLowerCase() === component.projectName.toLowerCase()       // Se busca el proyecto cuyo nombre coincide con el component.projectName
+    );
+
+    if(project) {                                                                // Si se encuentra, se establece el proyecto seleccionado
+      setSelectedProject(project);
+    }else{
+      console.log(`Project not found for component: ${component.name}`);
+    }
+
+    setSelectedComponent(component);                                             // Se establece el componente seleccionado
+    setOpenDeleteWindow(true)                                                    // Se abre la ventana de confirmación de eliminación
   }
 
   return (
@@ -57,7 +73,10 @@ const SingleFavoriteComponent = ({ component }: { component:Component }) => {
         >
           <EditIcon fontSize='small' className='text-white text-[13px]' />
         </div>
-        <div className='bg-sky-500 rounded-full w-7 h-7 flex items-center justify-center hover:bg-sky-600 cursor-pointer'>
+        <div 
+          onClick={openTheDeleteWindow}
+          className='bg-sky-500 rounded-full w-7 h-7 flex items-center justify-center hover:bg-sky-600 cursor-pointer'
+        >
           <DeleteIcon fontSize='small' className='text-white text-[13px]' />
         </div>
       </div>
