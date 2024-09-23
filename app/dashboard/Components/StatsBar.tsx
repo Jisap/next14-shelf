f4ec6eb5c39cdaf86c3ece3e8668b21005ee7200
Jorge Matos <jisapjisap@gmail.com>
2024-09-23 18:04:17 +0200

    statsBar

--- app/dashboard/Components/StatsBar.tsx
+++ app/dashboard/Components/StatsBar.tsx
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import AccountTreeIcon from '@mui/icons-material/AccountTree';
 import CategoryIcon from '@mui/icons-material/Category';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import CategoriesCard from "./CategoriesCard";
+import { useAppContext } from "@/app/ContextApi";
 
 
 
@@ -15,27 +16,37 @@ export interface StatisticCard {
 
 const StatsBar = () => {
 
-  const [statisticsCards, setStatisticsCard] = useState<StatisticCard[]>([
-    {
-      id: 1,
-      name: "Projects Created",
-      icon: <AccountTreeIcon className="text-sky-400" />,
-      count: 3,
-    },
-    {
-      id: 2,
-      name: "Components Added",
-      icon: <CategoryIcon className="text-sky-400" />,
-      count: 12,
-    },
-    {
-      id: 3,
-      name: "Favorite components",
-      icon: <FavoriteIcon className="text-sky-400" />,
-      count: 3
-    },
-  ])
+  const [statisticsCards, setStatisticsCard] = useState<StatisticCard[]>([])
 
+  const {
+    allProjectsObject: { allProjects },
+    allFavoriteComponentsObject: { allFavoriteComponents },
+    isLoadingObject: { isLoading },
+  } = useAppContext();
+
+  useEffect(() => {
+    setStatisticsCard([
+      {
+        id: 1,
+        name: "Projects Created",
+        icon: <AccountTreeIcon className="text-sky-400" />,
+        count: allProjects.length,
+      },
+      {
+        id: 2,
+        name: "Components Added",
+        icon: <CategoryIcon className="text-sky-400" />,
+        count: allProjects.reduce((acc, project) => acc + project.components.length, 0),
+      },
+      {
+        id: 3,
+        name: "Favorite components",
+        icon: <FavoriteIcon className="text-sky-400" />,
+        count: allFavoriteComponents.length,
+      },
+    ])
+  },[allProjects, allFavoriteComponents])
+ 
   return (
     <div className="mt-8">
       <div className="grid grid-cols-3 gap-4 rounded-lg mt-2">
