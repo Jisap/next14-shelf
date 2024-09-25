f0cc321c0d46b8b7967ecb482fd73a1ba48e9834
Jorge Matos <jisapjisap@gmail.com>
2024-09-25 19:51:26 +0200

    searchProjecWindow

--- app/dashboard/Components/SearchBarProjectsWindow.tsx
+++ app/dashboard/Components/SearchBarProjectsWindow.tsx
@@ -5,11 +5,12 @@ import CloseIcon from '@mui/icons-material/Close';
 
 
 const SearchBarProjectsWindow = ({
-  searchQuery, 
+  searchQuery,
   setSearchQuery
-}:{
-  searchQuery: string, 
-  setSearchQuery: React.Dispatch<React.SetStateAction<string>>}) => {
+}: {
+  searchQuery: string,
+  setSearchQuery: React.Dispatch<React.SetStateAction<string>>
+}) => {
 
   const { openAllProjectsWindowObject: { openAllProjectsWindow, setOpenAllProjectsWindow } } = useAppContext();
 
@@ -17,46 +18,50 @@ const SearchBarProjectsWindow = ({
 
   useEffect(() => {
     // Focus the input only when openAllProjectsWindow is open ( true )
-    if(openAllProjectsWindow){
+    if (openAllProjectsWindow) {
       const focusInput = () => {
-        if(inputRef.current){
+        if (inputRef.current) {
           inputRef.current.focus()
         }
       };
 
-      if(!openAllProjectsWindow){
+      if (!openAllProjectsWindow) {
         //Schedule focus setting for the next render
         setTimeout(focusInput, 0)
       }
     }
-  },[openAllProjectsWindow, searchQuery])
+  }, [openAllProjectsWindow, searchQuery])
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearchQuery(event.target.value)
   }
 
   return (
-    <div className="flex-gap-5 items-center justify-between mt-12 relative">
-      <div className={`h-[42px] bg-slate-50 flex items-center text-sm rounded-md pl-3 gap-1 w-[85%]`}>
-        <SearchRounded className="text-slate-400" />
-        <input 
-          ref={inputRef}
-          value={searchQuery}
-          onChange={handleChange}
-          placeholder="Search a Project..."
-          className="bg-transparent outline-none w-full font-light"
-        />
-        {/* Close Icon */}
-        {searchQuery.length > 0 && (
-          <div
-            onClick={() => setSearchQuery("")}
-            className="text-slate-400 cursor-pointer absolute right-2 top-3"
-          >
-            <CloseIcon 
-              sx={{ fontSize: 17 }}
-            />
-          </div>
-        )}
+    <div className="flex gap-5 items-center  mt-12 relative">
+      <div className="flex w-full items-center">
+        <div className={`h-[42px] bg-slate-50 flex justify-between w-full items-center text-sm rounded-md pl-3 gap-1  relative`}>
+          <SearchRounded className="text-slate-400" />
+
+          <input
+            ref={inputRef}
+            value={searchQuery}
+            onChange={handleChange}
+            placeholder="Search a Project..."
+            className="bg-transparent outline-none w-full font-light"
+          />
+
+          {/* Close Icon */}
+          {searchQuery.length > 0 && (
+            <div
+              onClick={() => setSearchQuery("")}
+              className="text-slate-400 cursor-pointer absolute right-2 top-3"
+            >
+              <CloseIcon
+                sx={{ fontSize: 17 }}
+              />
+            </div>
+          )}
+        </div>
         <button
           className="bg-sky-500 ml-2 p-[10px] flex w-[15%] text-sm rounded-md text-white items-center justify-between"
         >
