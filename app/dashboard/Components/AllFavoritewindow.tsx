94ba609e486e6e90407bbe97f367fceb5af7b1c6
Jorge Matos <jisapjisap@gmail.com>
2024-10-03 18:20:05 +0200

    HeaderFavoriteWindow

---
+++ app/dashboard/Components/AllFavoritewindow.tsx
@@ -0,0 +1,18 @@
+import HeaderFavoriteWindow from "./HeaderFavoriteWindow"
+import SearchBar from "./SearchBar"
+
+
+
+
+function AllFavoritewindow() {
+  return (
+    <div className="w-[77%] max-sm:w-[90%] p-9 border border-slate-50 h-[700px] bg-white rounded-xl shadow-md absolute left-1/2 lg:left-[58%] top-20 -translate-x-1/2 z-50">
+      <HeaderFavoriteWindow />
+      {/* <SearchBarFavoriteWindow />
+      <ComponentsNumber />
+      <ComponentsList /> */}
+    </div>
+  )
+}
+
+export default AllFavoritewindow
--- app/dashboard/Components/AllProjectsWindow.tsx
+++ app/dashboard/Components/AllProjectsWindow.tsx
@@ -19,7 +19,7 @@ const AllProjectsWindow = () => {
   return (
     <div
       style={{ display: openAllProjectsWindow ? "block" : "none" }}
-      className="w-[70%] max-w-sm:w-[90%] p-9 border border-slate-50 h-[82%] bg-white rouded-xl shadow-md absolute left-1/2 lg:left-[58%] top-20 -translate-x-1/2 z-50"
+      className="w-[70%] max-w-sm:w-[90%] p-9 border border-slate-50 h-[82%] bg-white rounded-xl shadow-md absolute left-1/2 lg:left-[58%] top-20 -translate-x-1/2 z-50"
     >
       <HeaderProjectWindow />
       <SearchBarProjectsWindow searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
