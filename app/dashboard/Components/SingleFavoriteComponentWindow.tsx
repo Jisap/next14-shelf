27d42ad43bddd0ed3fb61137bd4e344b3a0a5a05
Jorge Matos <jisapjisap@gmail.com>
2024-10-06 16:18:03 +0200

    SingleFavoriteComponent

--- app/dashboard/Components/SingleFavoriteComponentWindow.tsx
+++ app/dashboard/Components/SingleFavoriteComponentWindow.tsx
@@ -1,4 +1,5 @@
 
+import { Delete, EditRounded } from '@mui/icons-material';
 import { Component } from '../../allData';
 
 const SingleFavoriteComponentWindow = ({ item }: {item: Component}) => {
@@ -18,6 +19,32 @@ const SingleFavoriteComponentWindow = ({ item }: {item: Component}) => {
             <span className='text-[11px] p-1 px-2 bg-sky-100  text-sky-500 rounded-lg'>
               {item.projectName}
             </span>
+          </div>
+        </div>
+      </div>
+
+      {/* Action Buttons */}
+      <div className='flex gap-2 items-center'>
+        {/* Edit Button */}
+        <div
+          //onClick={editTheProjectClicked}
+          className='rounded-full w-7 h-7 flex items-center justify-center cursor-pointer bg-slate-200 hover:bg-slate-300'
+        >
+          <EditRounded
+            className='text-slate-400'
+            sx={{ fontSize: 15 }}
+          />
+        </div>
+        {/* Delete Button */}
+        <div>
+          <div
+            //onClick={openTheDeleteWindow}
+            className='rounded-full w-7 h-7 flex items-center justify-center cursor-pointer bg-slate-200 hover:bg-slate-300'
+          >
+            <Delete
+              className='text-slate-400'
+              sx={{ fontSize: 15 }}
+            />
           </div>
         </div>
       </div>
