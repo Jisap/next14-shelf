b5202441ea62a38071df60b95c8006b5e2062441
Jorge Matos <jisapjisap@gmail.com>
2024-10-11 16:51:27 +0200

    You are filter by

--- app/dashboard/Components/ComponentsNumber.tsx
+++ app/dashboard/Components/ComponentsNumber.tsx
@@ -1,4 +1,5 @@
 import { useAppContext } from "@/app/ContextApi"
+import { Close } from "@mui/icons-material"
 
 
 
@@ -8,6 +9,11 @@ const ComponentsNumber = () => {
 
   const {
     allFavoriteComponentsObject: { allFavoriteComponents },
+    isLoadingObject: {isLoading},
+    selectedProjectToFilterObject: {
+      selectedProjectToFilter,
+      setSelectedProjectToFilter
+    }
   } = useAppContext()
 
   return (
@@ -17,6 +23,19 @@ const ComponentsNumber = () => {
         <span className="text-sky-500 font-semibold">{allFavoriteComponents.length}</span>
         <span className="text-slate-400">components as favorite!</span>
       </div>
+      {selectedProjectToFilter && (
+        <div>
+          <span className="text-slate-400">You are filtering by :  </span>
+          <span className="text-[12px] rounded-lg bg-sky-100  text-sky-500 p-[6px] px-2">
+            {selectedProjectToFilter}
+            <Close 
+              onClick={() => setSelectedProjectToFilter("")}
+              sx={{ fontSize: 16 }}
+              className="text-sky-500 pl-1 cursor-pointer"
+            />
+          </span>
+        </div>
+      )}
     </div>
   )
 }
