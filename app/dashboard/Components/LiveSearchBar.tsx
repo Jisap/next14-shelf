cdc02ed84cedc1e8d408d37a9a0dbdd046110104
Jorge Matos <jisapjisap@gmail.com>
2024-10-13 16:21:12 +0200

    liveSearchBar

---
+++ app/dashboard/Components/LiveSearchBar.tsx
@@ -0,0 +1,17 @@
+
+
+const LiveSearchBar = () => {
+  return (
+    <div className="fixed p-5 w-[25%] flex flex-col gap-3 shadow-md border border-slate-50 bg-white rounded-lg top-14">
+      {/* Project Results */}
+      <div>
+        
+      </div>
+      {/* Component Results */}
+      <div>
+      </div>
+    </div>
+  )
+}
+
+export default LiveSearchBar
