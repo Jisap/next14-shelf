7d3b2a846165c877d16d763fcd8f54be90ba03d2
Jorge Matos <jisapjisap@gmail.com>
2024-10-14 15:26:30 +0200

    filteredProjects

--- app/dashboard/Components/LiveSearchBar.tsx
+++ app/dashboard/Components/LiveSearchBar.tsx
@@ -9,9 +9,22 @@ const LiveSearchBar = () => {
     openLiveSearchBarObject: { openLiveSearchBar, setOpenLiveSearchBar },
     liveSearchPositionsObject: { liveSearchPositions, setLiveSearchPositions },
     isMobileViewObject: { isMobileView },
+    mainSearchQueryObject: { mainSearchQuery },
+    allProjectsObject: { allProjects },
   } = useAppContext()
 
   const liveSearchBarRef = useRef<HTMLDivElement>(null);
+
+  //Filter projects and components bases on the search query
+  const filteredProjects = allProjects.filter((project) =>
+    project.name.toLowerCase().includes(mainSearchQuery.toLowerCase())
+  )
+
+  const filteredComponents = allProjects.flatMap((project) => 
+    project.components).filter((component) =>
+      component.name.toLowerCase().includes(mainSearchQuery.toLowerCase()
+    )
+  )
 
   return (
     <div 
@@ -24,29 +37,43 @@ const LiveSearchBar = () => {
         fixed p-5 
         ${isMobileView ? "w-[70%]" : "w-[26%]"} 
         w-[25%] flex flex-col gap-3 shadow-md border border-slate-50 bg-white rounded-lg top-14 left-96 z-50
-      `}>
-      {/* Project Results */}
-      <div>
-        <span className="font-bold text-[14px] text-slate-800">
-          Projects
-        </span>
+      `}
+      >
+        {filteredComponents.length === 0 && filteredProjects.length === 0 && (
+          <span className="text-slate-500 text-[12px]">
+            No match results...
+          </span>
+        )}
 
-        <div className="flex gap-1 mt-3 flex-col ml-1">
-          {/* Single Project */}
-          <div className="flex items-center gap-1 p-2 rounded-md hover:bg-slate-100 select-none cursor-pointer">
-            <div className="w-[21px] h-[21px] bg-sky-200 rounded-full flex items-center justify-center">
-              <CodeRounded 
-                sx={{ fontSize: "15px"}}
-                className="text-sky-500 text-[18px]"
-              />
-            </div>
-            <span className="text-[12px] text-slate-700">
-              Project Name
-            </span>
+      {/* Project Results */}
+      {filteredProjects.length > 0 &&(
+        <div>
+          <span className="font-bold text-[14px] text-slate-800">
+            Projects
+          </span>
+
+          <div className="flex gap-1 mt-3 flex-col ml-1">
+            {/* Single Project */}
+            {filteredProjects.slice(0,3).map((project) => (
+              <div 
+                key={project._id}
+                className="flex items-center gap-1 p-2 rounded-md hover:bg-slate-100 select-none cursor-pointer"
+              >
+                <div className="w-[21px] h-[21px] bg-sky-200 rounded-full flex items-center justify-center">
+                  <CodeRounded 
+                    sx={{ fontSize: "15px"}}
+                    className="text-sky-500 text-[18px]"
+                  />
+                </div>
+                <span className="text-[12px] text-slate-700">
+                  {project.name}
+                </span>
+              </div>
+            ))}
+            
           </div>
-          
         </div>
-      </div>
+      )}
 
       {/* Component Results */}
       <div>
